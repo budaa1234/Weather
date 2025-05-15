21384c1d5c4da57371566538a84042246250fde6
budaa1234 <budod1234@gmail.com>
2025-05-15 13:29:09 +0800

    weath

---
+++ src/Components/User.jsx
@@ -0,0 +1,10 @@
+export const User = () => {
+  return (
+    <div className="flex items-center justify-center gap-x-10 mb-40">
+      <img src="Home.png" />
+      <img src="Vector.png" />
+      <img src="Stroke.png" />
+      <img src="User.png" />
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,88 +1,98 @@
 import { Sun } from "@/Components/Sun";
 import { Moon } from "@/Components/Moon";
-import { Sum } from "@/Components/Sum";
-import { useState } from "react";
+import { User } from "@/Components/User";
+import { Circle } from "@/Components/circle";
+
+import { useEffect, useState } from "react";
 export default function Home() {
   const [weather, setWeather] = useState({});
+  const [city, setCity] = useState("ulaanbaatar");
 
-  const cityUrl = `https://api.api-ninjas.com/v1/city?name=Ulaanbaatar`;
+  const inter = (e) => setCity(e.target.value);
 
-  const getCity = async () => {
-    try {
-      const response = await fetch(cityUrl, {
-        headers: {
-          "X-Api-Key": "JAmSPW3U/yuidAkPKuBopg==EhSpVGxOD4Ck9ZQ7",
-        },
-      });
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
   const getWeather = async () => {
     try {
-      const cityLocation = await getCity();
-
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${cityLocation[0].latitude}&lon=${cityLocation[0].longitude}&appid=b70b0a8300df32bea5bb56e1a9d00a88`
+        `https://api.weatherapi.com/v1/forecast.json?key=e6600c3f220e429fa5e25438251505&q=${city}`
       );
 
       const data = await response.json();
-      setWeather(data)
+      console.log(data);
+      setWeather(data);
     } catch (error) {
       console.log(error);
     }
   };
-  console.log("first", weather);
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
-    // <div>
-    //   <button onClick={getWeather}>get</button>
-    //   <p>date:{weather.name}</p>
-    //   <p>temp:{weather.main?.temp}</p>
-    // </div>
     <div className="flex min-h-screen">
-      <div className="absolute flex items-center justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-300 rounded-full w-[140px] h-[140px]">
-        <Sum />
-      </div>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-300 rounded-full w-[540px] h-[540px]"></div>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-300 rounded-full w-[940px] h-[940px]"></div>
       {/* zuun */}
-      <div className="relative flex flex-1/2 items-center justify-center gap-y-[20px]">
-        <div className="relative flex w-[567px] justify-center ">
-          <div className="w-103 h-207 rounded-[48px] overflow-hidden shadow-lg bg-[#F3F4F6]">
+
+      <div className="relative flex flex-1/2 items-center justify-center">
+        <div className=" relative flex-col w-[567px] justify-center z-20">
+          <div className="flex w-full  pl-[30px]  -top-16 text-[30px] bg-[#FFFFFF] shadow-md rounded-[48px]">
             <input
+              onChange={inter}
               type="text"
               placeholder="Search"
-              className="absolute  right-[70px] pl-[30px] w-full -top-16 text-[32px] bg-[#FFFFFF] shadow-md rounded-[48px] "
+              className=" "
             />
-            <div className="flex-col items-center justify-center gap-y-20">
-              <p className="p-[40px] h-12 text-3xl font-extrabold text-gray-900">
-                {weather.name}
-              </p>
-              <Sun />
-              <p className="p-[40px] text-transparent bg-clip-text font-extrabold text-[100px] -mt-10 bg-gradient-to-b from-black to-white ">
-                {weather.main?.temp}
-              </p>
-              <button className="border" onClick={getWeather} >get</button>
+            <div className=" pt-3 rounded-[38px]">
+              <button onClick={getWeather}>Search</button>
+            </div>
+          </div>
+          <div className="relative flex w-[567px] justify-center z-10">
+            <div className=" z-30 w-103 h-207 rounded-[48px] overflow-hidden shadow-lg bg-[#F3F4F6]">
+              <div className="flex-col items-center justify-center gap-y-20">
+                <p className="pl-[40px] text-[#9CA3AF]">
+                  {weather.forecast?.forecastday[0]?.date}
+                </p>
+                <p className="p-[40px] h-12 text-3xl font-extrabold text-gray-900">
+                  {weather.location?.name}
+                </p>
+                <Sun />
+                <p className="p-[40px] text-transparent bg-clip-text font-extrabold text-[80px] -mt-10 bg-gradient-to-b from-black to-white ">
+                  {weather.forecast?.forecastday[0]?.day?.maxtemp_c}°C
+                </p>
+                <h6 className="font-extrabold h-6 text-[#FF8E27] p-[40px]">
+                  {weather.forecast?.forecastday[0]?.day?.condition?.text}
+                </h6>
+                <User />
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+      <Circle />
+
       {/* baruun */}
+
+
       <div className="relative flex flex-1/2 items-center justify-center bg-[#0F141E]">
-        <div className="relative flex w-[567px] justify-center z-10">
-          <div className="z-20 w-103 h-207 rounded-[48px] overflow-hidden shadow-lg bg-[#111827]/75">
+        <div className="relative flex w-[567px] justify-center z-10 pt-[57px]">
+          <div className="z-20 w-103 h-207 rounded-[48px] overflow-hidden shadow-lg bg-[#111827]/75 backdrop-blur-md">
+            <p className="pl-[40px] text-[#9CA3AF]">
+              {weather.forecast?.forecastday[0]?.date}
+            </p>
             <p className=" p-[40px] h-12 text-3xl font-extrabold text-[#FFFFFF]">
-              {weather.name}
+              {weather.location?.name}
             </p>
             <Moon />
-            <p className="p-[40px]  text-transparent bg-clip-text font-extrabold text-[100px] -mt-10 bg-gradient-to-b from-black to-white ">
-              {weather.main?.temp}
+            <p className="p-[40px]  text-transparent bg-clip-text font-extrabold text-[80px] -mt-10 bg-gradient-to-b from-black to-white ">
+              {weather.forecast?.forecastday[0]?.day?.mintemp_c}°C
             </p>
+            <h6 className="font-extrabold h-6 text-[#777CCE] p-[40px]">
+              {weather.forecast?.forecastday[0]?.day?.condition?.text}
+            </h6>
+            <User />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
